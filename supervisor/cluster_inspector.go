6720447cd8d367847a78bf1d4f80189c07649f35
Alberto Gregoris <alberto.gregoris@intelygenz.com>
2014-10-16 10:03:53 +0200

    Cluster inspector first implementation (tested)

--- supervisor/cluster_inspector.go
+++ supervisor/cluster_inspector.go
@@ -5,72 +5,106 @@ type ClusterAnalyzer interface {
 }
 
 const (
-	// TODO: unify etcd constants
-	LeaderKey string = "leader"
+	LeaderKey   string = "leader"
+	MachinesKey string = "machines"
 )
 
 type ClusterInspector struct {
 	EtcdClient   EtcdRequester
 	configPeers  []string
-	peerCluster  PeerCluster
+	myPeerAddr   string
+	PeerCluster  *PeerCluster
 	PeersMonitor FolderMonitor
 }
 
-// TODO: etcdClient not necessary
-func NewClusterInspector(selfAddr, selfPeerAddr string, knownPeers []string) *ClusterInspector {
+func NewClusterInspector(ownAddr, ownPeerAddr string, knownPeers []string) *ClusterInspector {
 	return &ClusterInspector{
-		EtcdClient:   NewEtcdClient([]string{selfAddr}),
 		configPeers:  []string{},
-		PeersMonitor: NewPeersMonitor(NewEtcdClient([]string{selfAddr}).WithMachineAddr(selfAddr)),
+		EtcdClient:   NewEtcdClient([]string{ownAddr}),
+		myPeerAddr:   ownPeerAddr,
+		PeerCluster:  NewPeerCluster([]Peer{}),
+		PeersMonitor: NewPeersMonitor(NewEtcdClient([]string{ownAddr}).WithMachineAddr(ownAddr)),
 	}
 }
 
-func (c *ClusterInspector) Run() {
+func (c *ClusterInspector) Run(ch chan string) {
 	var newPeers []Peer
 	peersMonitorChannel := make(chan []Peer)
 	go c.PeersMonitor.Run(peersMonitorChannel)
-	// OuterLoop:
+OuterLoop:
 	for {
 		select {
-		// case <-e.executionChannel:
-		// 	fmt.Println("------->>> BREAK LOOP")
-		// 	break OuterLoop
-		// case peerCluster = <-peersMonitorChannel:
 		case newPeers = <-peersMonitorChannel:
-			c.peerCluster.SetPeers(newPeers)
+			c.PeerCluster.Peers = newPeers
 		default:
-			// leader := c.searchForLeader()
-			_ = c.searchForLeader()
-			// time.Sleep(e.durationBetweenPublicationsState)
+			leader := c.searchForLeader()
+			if leader != nil {
+				foreignPeerCluster, err := c.getCluster(AddHttpProtocol((*leader).Addr))
+				if err == nil {
+					if c.shouldTryToJoinTheCluster((*leader).PeerAddr, foreignPeerCluster) {
+						ch <- AddHttpProtocol((*leader).PeerAddr)
+						break OuterLoop
+					}
+				}
+			}
+			// TODO: Add sleep
 		}
 	}
 }
 
-func (c *ClusterInspector) mergeCluster() {
-
+func (c *ClusterInspector) shouldTryToJoinTheCluster(foreignLeaderPeerAddr string, foreignCluster *PeerCluster) bool {
+	if len(foreignCluster.GetEnabledPeers()) > len(c.PeerCluster.GetEnabledPeers()) {
+		return true
+	} else if len(foreignCluster.GetEnabledPeers()) < len(c.PeerCluster.GetEnabledPeers()) {
+		return false
+	} else if c.iHavePriorityOverCluster(foreignLeaderPeerAddr, foreignCluster) {
+		return false
+	}
+	return true
 }
 
-func (c *ClusterInspector) searchForLeader() string {
-	// TODO: Reset iterator
-	for /* reset iterator*/ c.peerCluster.HasNext() {
-		peer, _ := c.peerCluster.Next()
-		peerLeader, err := c.getPeerLeader(peer.Addr)
-		if err == nil && peerLeader != "" {
+func (c *ClusterInspector) iHavePriorityOverCluster(foreignLeaderPeerAddr string, foreignCluster *PeerCluster) bool {
+	myPositionInForeignCluster, err := foreignCluster.GetPeerPosition(c.myPeerAddr)
+	if err != nil {
+		return false
+	}
+	foreignLeaderPositionInOwnCluster, err := c.PeerCluster.GetPeerPosition(foreignLeaderPeerAddr)
+	if err != nil {
+		return true
+	}
+	if myPositionInForeignCluster <= foreignLeaderPositionInOwnCluster {
+		return true
+	}
+	return false
+}
 
+func (c *ClusterInspector) getCluster(addr string) (*PeerCluster, error) {
+	c.EtcdClient.WithMachineAddr(addr)
+	res, err := c.EtcdClient.Get(ClusterKey, true, true)
+	if err != nil {
+		return nil, err
+	}
+	return NewPeerClusterFromNodes(res.Node.Nodes), nil
+}
+
+func (c *ClusterInspector) searchForLeader() *Peer {
+	for c.PeerCluster.Reset(); c.PeerCluster.HasNext(); {
+		peer, _ := c.PeerCluster.Next()
+		addrURL := AddHttpProtocol(peer.Addr)
+		peerLeader, err := c.getLeader(addrURL)
+		if err == nil && peerLeader == AddHttpProtocol(peer.PeerAddr) {
+			return &peer
 		}
 	}
-	return ""
+	return nil
 }
 
-func (c *ClusterInspector) getPeerLeader(addr string) (string, error) {
-	res, err := c.EtcdClient.WithMachineAddr(addr).BaseGet(LeaderKey)
+func (c *ClusterInspector) getLeader(addr string) (string, error) {
+	c.EtcdClient.WithMachineAddr(addr)
+	res, err := c.EtcdClient.BaseGet(LeaderKey)
 	if err != nil {
 		return "", err
 	}
 	leader := string(res.Body)
 	return leader, nil
-}
-
-func (c *ClusterInspector) refreshCluster() {
-	// res, err := c.etcdClient.WithMachineAddr(peerAddr).BaseGet(LeaderKey)
 }
--- supervisor/cluster_inspector_test.go
+++ supervisor/cluster_inspector_test.go
@@ -6,21 +6,21 @@ import (
 
 	"github.com/innotech/hydra/vendors/code.google.com/p/gomock/gomock"
 	. "github.com/innotech/hydra/vendors/github.com/onsi/ginkgo"
-	// . "github.com/innotech/hydra/vendors/github.com/onsi/gomega"
+	. "github.com/innotech/hydra/vendors/github.com/onsi/gomega"
 
-	// "errors"
+	"errors"
 	"net/http"
 	"time"
 )
 
-var _ = FDescribe("ClusterInspector", func() {
+var _ = Describe("ClusterInspector", func() {
 	const (
 		etcdAddrItself string = "127.0.0.1:7401"
 		peerAddrItself string = "127.0.0.1:7701"
 	)
 
 	var (
-		// ch               chan PeerCluster
+		ch               chan string
 		mockCtrl         *gomock.Controller
 		mockEtcdClient   *mock.MockEtcdRequester
 		mockPeersMonitor *mock.MockFolderMonitor
@@ -34,131 +34,382 @@ var _ = FDescribe("ClusterInspector", func() {
 		clusterInspector = NewClusterInspector(etcdAddrItself, peerAddrItself, []string{})
 		clusterInspector.EtcdClient = mockEtcdClient
 		clusterInspector.PeersMonitor = mockPeersMonitor
-		// ch = make(chan StateControllerState)
+		ch = make(chan string)
 	})
 
 	AfterEach(func() {
 		mockCtrl.Finish()
 	})
 
-	successGetLeaderResponse := &RawResponse{
-		StatusCode: http.StatusOK,
-		Body:       []byte("http://" + peerAddrItself),
-		Header:     nil,
+	var modifiedIndex uint64 = 3
+	makeGetClusterResponse := func(nodes []*Node) *Response {
+		return &Response{
+			Action: "",
+			Node: &Node{
+				Key:           ClusterKey,
+				Value:         "",
+				Dir:           true,
+				Expiration:    nil,
+				TTL:           3,
+				Nodes:         nodes,
+				ModifiedIndex: modifiedIndex,
+				CreatedIndex:  modifiedIndex - 1,
+			},
+			PrevNode:  nil,
+			EtcdIndex: 0,
+			RaftIndex: 0,
+			RaftTerm:  0,
+		}
 	}
+
+	peer0 := Peer{
+		Addr:     "98.245.153.111:4001",
+		PeerAddr: "98.245.153.111:7001",
+		State:    PeerStateEnabled,
+	}
+	peer1 := Peer{
+		Addr:     "98.245.153.112:4001",
+		PeerAddr: "98.245.153.112:7001",
+		State:    PeerStateEnabled,
+	}
+	peer2 := Peer{
+		Addr:     "98.245.153.113:4001",
+		PeerAddr: "98.245.153.113:7001",
+		State:    PeerStateEnabled,
+	}
+	expectedPeers := []Peer{peer0, peer1}
+
+	node0 := &Node{
+		Key:        peer0.PeerAddr,
+		Value:      "",
+		Dir:        true,
+		Expiration: nil,
+		TTL:        3,
+		Nodes: []*Node{
+			&Node{
+				Key:           AddrKey,
+				Value:         peer0.Addr,
+				Dir:           false,
+				Expiration:    nil,
+				TTL:           3,
+				Nodes:         nil,
+				ModifiedIndex: modifiedIndex,
+				CreatedIndex:  modifiedIndex - 1,
+			},
+			&Node{
+				Key:           StateKey,
+				Value:         peer0.State,
+				Dir:           false,
+				Expiration:    nil,
+				TTL:           3,
+				Nodes:         nil,
+				ModifiedIndex: modifiedIndex,
+				CreatedIndex:  modifiedIndex - 1,
+			},
+		},
+		ModifiedIndex: modifiedIndex,
+		CreatedIndex:  modifiedIndex - 1,
+	}
+	node1 := &Node{
+		Key:        peer1.PeerAddr,
+		Value:      "",
+		Dir:        true,
+		Expiration: nil,
+		TTL:        3,
+		Nodes: []*Node{
+			&Node{
+				Key:           AddrKey,
+				Value:         peer1.Addr,
+				Dir:           false,
+				Expiration:    nil,
+				TTL:           3,
+				Nodes:         nil,
+				ModifiedIndex: modifiedIndex,
+				CreatedIndex:  modifiedIndex - 1,
+			},
+			&Node{
+				Key:           StateKey,
+				Value:         peer1.State,
+				Dir:           false,
+				Expiration:    nil,
+				TTL:           3,
+				Nodes:         nil,
+				ModifiedIndex: modifiedIndex,
+				CreatedIndex:  modifiedIndex - 1,
+			},
+		},
+		ModifiedIndex: modifiedIndex,
+		CreatedIndex:  modifiedIndex - 1,
+	}
+	node2 := &Node{
+		Key:        peer2.PeerAddr,
+		Value:      "",
+		Dir:        true,
+		Expiration: nil,
+		TTL:        3,
+		Nodes: []*Node{
+			&Node{
+				Key:           AddrKey,
+				Value:         peer2.Addr,
+				Dir:           false,
+				Expiration:    nil,
+				TTL:           3,
+				Nodes:         nil,
+				ModifiedIndex: modifiedIndex,
+				CreatedIndex:  modifiedIndex - 1,
+			},
+			&Node{
+				Key:           StateKey,
+				Value:         peer2.State,
+				Dir:           false,
+				Expiration:    nil,
+				TTL:           3,
+				Nodes:         nil,
+				ModifiedIndex: modifiedIndex,
+				CreatedIndex:  modifiedIndex - 1,
+			},
+		},
+		ModifiedIndex: modifiedIndex,
+		CreatedIndex:  modifiedIndex - 1,
+	}
+
+	cErr := errors.New("Unreachable peer")
 
 	Describe("Run", func() {
 		It("should run a Peers Monitor", func() {
 			mockPeersMonitor.EXPECT().Run(gomock.Any()).Times(1)
 
 			go func() {
-				clusterInspector.Run()
+				clusterInspector.Run(ch)
 			}()
 			time.Sleep(time.Duration(1) * time.Second)
 		})
+		Context("when peers monitor emit new cluster", func() {
+			It("should update the peer cluster", func() {
+				var peersMonitorChannel chan []Peer
+				mockPeersMonitor.EXPECT().Run(gomock.Any()).Do(func(ch chan []Peer) {
+					peersMonitorChannel = ch
+				}).Times(1)
+				mockEtcdClient.EXPECT().WithMachineAddr(gomock.Any()).
+					AnyTimes()
+				mockEtcdClient.EXPECT().BaseGet(gomock.Any()).
+					Return(nil, cErr).AnyTimes()
+				go func() {
+					clusterInspector.Run(ch)
+				}()
+				Expect(clusterInspector.PeerCluster.Peers).To(Equal([]Peer{}))
+				time.Sleep(time.Duration(200) * time.Millisecond)
+				peersMonitorChannel <- expectedPeers
+				Expect(clusterInspector.PeerCluster.Peers).To(Equal(expectedPeers))
+			})
+		})
 		It("should search for a peer to connect", func() {
-			mockPeersMonitor.EXPECT().Run(gomock.Any()).AnyTimes()
-			c1 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).Return(successGetLeaderResponse, nil).Times(1)
-			mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).Return(successGetLeaderResponse, nil).AnyTimes().After(c1)
+			clusterInspector.PeerCluster.Peers = expectedPeers
+			mockPeersMonitor.EXPECT().Run(gomock.Any()).Times(1)
+			mockEtcdClient.EXPECT().WithMachineAddr(gomock.Any()).AnyTimes()
+			c1 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+				Return(nil, cErr).Times(1)
+			mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+				Return(nil, cErr).AnyTimes().After(c1)
 
 			go func() {
-				clusterInspector.Run()
+				clusterInspector.Run(ch)
 			}()
-			time.Sleep(time.Duration(3) * time.Second)
+			time.Sleep(time.Duration(1) * time.Second)
+		})
+		Context("when none peer is reachable", func() {
+			It("should consult for all peers and repeat it", func() {
+				clusterInspector.PeerCluster.Peers = expectedPeers
+				mockPeersMonitor.EXPECT().Run(gomock.Any()).Times(1)
+
+				addr0 := AddHttpProtocol(expectedPeers[0].Addr)
+				addr1 := AddHttpProtocol(expectedPeers[1].Addr)
+				c1 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addr0)).
+					Times(1)
+				c2 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+					Return(nil, cErr).Times(1).After(c1)
+				c3 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addr1)).
+					Times(1).After(c2)
+				c4 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+					Return(nil, cErr).Times(1).After(c3)
+				c5 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addr0)).
+					Times(1).After(c4)
+				c6 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+					Return(nil, cErr).Times(1).After(c5)
+				mockEtcdClient.EXPECT().WithMachineAddr(gomock.Any()).
+					AnyTimes().After(c6)
+				mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+					Return(nil, cErr).AnyTimes().After(c6)
+
+				go func() {
+					clusterInspector.Run(ch)
+				}()
+				time.Sleep(time.Duration(1) * time.Second)
+			})
+		})
+		Context("when find a foreign peer to connect", func() {
+			Context("when reachable peer is not leader", func() {
+				It("should continue searching a peer to connect", func() {
+					clusterInspector.PeerCluster.Peers = expectedPeers
+					mockPeersMonitor.EXPECT().Run(gomock.Any()).Times(1)
+
+					addr0 := AddHttpProtocol(expectedPeers[0].Addr)
+					c1 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addr0)).
+						Times(1)
+					res := &RawResponse{
+						StatusCode: http.StatusOK,
+						Body:       []byte("http://" + peerAddrItself),
+						Header:     nil,
+					}
+					c2 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+						Return(res, nil).Times(1).After(c1)
+					mockEtcdClient.EXPECT().WithMachineAddr(gomock.Any()).
+						AnyTimes().After(c2)
+					mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+						Return(nil, cErr).AnyTimes().After(c2)
+
+					go func() {
+						clusterInspector.Run(ch)
+					}()
+					time.Sleep(time.Duration(1) * time.Second)
+				})
+			})
+			Context("when reachable peer is leader", func() {
+				Context("when size of foreign cluster is less than own", func() {
+					It("should continue searching for leader", func() {
+						clusterInspector.PeerCluster.Peers = expectedPeers
+						addrURL := AddHttpProtocol(expectedPeers[0].Addr)
+						peerURL := AddHttpProtocol(expectedPeers[0].PeerAddr)
+						getLeaderResponse := &RawResponse{
+							StatusCode: http.StatusOK,
+							Body:       []byte(peerURL),
+							Header:     nil,
+						}
+						nodes := []*Node{node0}
+						getClusterResponse := makeGetClusterResponse(nodes)
+						mockPeersMonitor.EXPECT().Run(gomock.Any()).Times(1)
+						c1 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addrURL)).
+							Times(1)
+						c2 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+							Return(getLeaderResponse, nil).Times(1).After(c1)
+						c3 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addrURL)).
+							Times(1).After(c2)
+						endCh := make(chan bool)
+						c4 := mockEtcdClient.EXPECT().Get(gomock.Eq(ClusterKey), gomock.Eq(true), gomock.Eq(true)).
+							Do(func(_ interface{}, _ interface{}, _ interface{}) {
+							endCh <- true
+						}).
+							Return(getClusterResponse, nil).Times(1).After(c3)
+						mockEtcdClient.EXPECT().WithMachineAddr(gomock.Any()).
+							AnyTimes().After(c4)
+						mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+							Return(nil, cErr).AnyTimes().After(c4)
+
+						go func() {
+							clusterInspector.Run(ch)
+						}()
+						<-endCh
+					})
+				})
+				Context("when size of foreign cluster is greater than own", func() {
+					It("should emit new leader found", func() {
+						clusterInspector.PeerCluster.Peers = expectedPeers
+						addrURL := AddHttpProtocol(expectedPeers[0].Addr)
+						peerURL := AddHttpProtocol(expectedPeers[0].PeerAddr)
+						getLeaderResponse := &RawResponse{
+							StatusCode: http.StatusOK,
+							Body:       []byte(peerURL),
+							Header:     nil,
+						}
+						nodes := []*Node{node0, node1, node2}
+						getClusterResponse := makeGetClusterResponse(nodes)
+						mockPeersMonitor.EXPECT().Run(gomock.Any()).Times(1)
+						c1 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addrURL)).
+							Times(1)
+						c2 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+							Return(getLeaderResponse, nil).Times(1).After(c1)
+						c3 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addrURL)).
+							Times(1).After(c2)
+						mockEtcdClient.EXPECT().Get(gomock.Eq(ClusterKey), gomock.Eq(true), gomock.Eq(true)).
+							Return(getClusterResponse, nil).Times(1).After(c3)
+
+						go func() {
+							clusterInspector.Run(ch)
+						}()
+						Expect(<-ch).To(Equal(peerURL))
+					})
+				})
+				Context("when size of foreign cluster is equal than own", func() {
+					Context("when foreign leader has less priority than it", func() {
+						It("should continue searching for leader", func() {
+							clusterInspector.PeerCluster.Peers = expectedPeers
+							addrURL := AddHttpProtocol(expectedPeers[1].Addr)
+							peerURL := AddHttpProtocol(expectedPeers[1].PeerAddr)
+							getLeaderResponse := &RawResponse{
+								StatusCode: http.StatusOK,
+								Body:       []byte(peerURL),
+								Header:     nil,
+							}
+							nodes := []*Node{node0, node1}
+							getClusterResponse := makeGetClusterResponse(nodes)
+							mockPeersMonitor.EXPECT().Run(gomock.Any()).Times(1)
+							c1 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(AddHttpProtocol(expectedPeers[0].Addr))).
+								Times(1)
+							c2 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+								Return(nil, cErr).Times(1).After(c1)
+							c3 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addrURL)).
+								Times(1).After(c2)
+							c4 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+								Return(getLeaderResponse, nil).Times(1).After(c3)
+							c5 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addrURL)).
+								Times(1).After(c4)
+							c6 := mockEtcdClient.EXPECT().Get(gomock.Eq(ClusterKey), gomock.Eq(true), gomock.Eq(true)).
+								Return(getClusterResponse, nil).Times(1).After(c5)
+							mockEtcdClient.EXPECT().WithMachineAddr(gomock.Any()).
+								AnyTimes().After(c6)
+							mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+								Return(nil, cErr).AnyTimes().After(c6)
+
+							go func() {
+								clusterInspector.Run(ch)
+							}()
+							time.Sleep(time.Duration(1) * time.Second)
+						})
+					})
+					Context("when foreign leader has greater priority than it", func() {
+						It("should emit new leader found", func() {
+							clusterInspector.PeerCluster.Peers = []Peer{expectedPeers[1:2][0], expectedPeers[0:1][0]}
+							addrURL := AddHttpProtocol(expectedPeers[1].Addr)
+							peerURL := AddHttpProtocol(expectedPeers[1].PeerAddr)
+							getLeaderResponse := &RawResponse{
+								StatusCode: http.StatusOK,
+								Body:       []byte(peerURL),
+								Header:     nil,
+							}
+							nodes := []*Node{node1, node0}
+							getClusterResponse := makeGetClusterResponse(nodes)
+							mockPeersMonitor.EXPECT().Run(gomock.Any()).Times(1)
+							c1 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addrURL)).
+								Times(1)
+							c2 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+								Return(getLeaderResponse, nil).Times(1).After(c1)
+							c3 := mockEtcdClient.EXPECT().WithMachineAddr(gomock.Eq(addrURL)).
+								Times(1).After(c2)
+							c4 := mockEtcdClient.EXPECT().Get(gomock.Eq(ClusterKey), gomock.Eq(true), gomock.Eq(true)).
+								Return(getClusterResponse, nil).Times(1).After(c3)
+							mockEtcdClient.EXPECT().WithMachineAddr(gomock.Any()).
+								AnyTimes().After(c4)
+							mockEtcdClient.EXPECT().BaseGet(gomock.Eq(LeaderKey)).
+								Return(nil, cErr).AnyTimes().After(c4)
+
+							go func() {
+								clusterInspector.Run(ch)
+							}()
+							Expect(<-ch).To(Equal(peerURL))
+						})
+					})
+				})
+			})
 		})
 	})
 })
-
-// **************************************************************************************************************
-
-// Context("when node find a foreign peer to connect", func() {
-// 	// It("should check if this peer is leader", func() {
-
-// 	// })
-// 	Context("when reachable peer is not leader", func() {
-// 		It("should continue searching a peer to connect", func() {
-// 			foreignPeerAddr := "98.245.153.111:7701"
-// 			// res := *RawResponse{
-// 			// 	StatusCode: http.StatusOK,
-// 			// 	Body:       []byte("http://" + peerAddrItself),
-// 			// 	Header:     nil,
-// 			// }
-// 			c1 := mockEtcdClient.EXPECT().WithMachineAddr("http://" + foreignPeerAddr).Times(1)
-// 			c2 := mockEtcdClient.EXPECT().BaseGet(gomock.Eq("leader")).Return(successGetLeaderResponse, nil).Times(1).After(c1)
-// 			mockEtcdClient.EXPECT().WithMachineAddr(gomock.Not("http://" + peerAddrItself)).AnyTimes().After(c2)
-// 			mockEtcdClient.EXPECT().BaseGet(gomock.Eq("leader")).Return(successGetLeaderResponse, nil).AnyTimes().After(c2)
-
-// 			go func() {
-// 				etcdSupervisor.Run()
-// 			}()
-// 			time.Sleep(time.Duration(2) * time.Second)
-// 		})
-// 	})
-// 	Context("when reachable peer is leader", func() {
-// 		// Next request for machines and compare with local cluster for overwrite local or remote cluster
-// 		It("should get registered peers from foreign peer", func() {
-// 			f1 := mockEtcdClient.EXPECT().GetLeader(gomock.Any()).
-// 				SetArg("http://98.245.153.111:7001").Return("http://98.245.153.111:7001", nil)
-// 			f2 := mockEtcdClient.EXPECT().Get(gomock.Any(), gomock.Any()).
-// 				SetArg("http://98.245.153.111:7001", "/cluster")
-
-// 			go func() {
-// 				etcdSupervisor.Run()
-// 			}()
-// 		})
-// 		Context("when the registered peers from foreign peer leader are retrieved", func() {
-// 			It("should add unknown peers to foreign leader", func() {
-
-// 			})
-// 		})
-
-// 		// TODO: priority = higher cluter size && node priority
-// 		Context("when it has lower cluster priority than reachable peer", func() {
-// 			It("should try connecting to reachable peer", func() {
-// 				successfulCallToSlaveNode := mockEtcdClient.EXPECT().GetLeader(gomock.Any()).SetArg("http://98.245.153.111:7001").Return("http://98.245.153.111:7001", nil)
-// 				mockEtcdClient.EXPECT().GetLeader(gomock.Any()).AnyTimes().After(successfulCallToSlaveNode)
-// 				// TODO: restart with attributes
-// 				mockEtcdManager.EXPECT().RestartEtcdService().Times(1).After(successfulCallToSlaveNode)
-
-// 				go func() {
-// 					etcdSupervisor.Run()
-// 				}()
-// 			})
-// 		})
-// 		Context("when it has higher cluster priority than reachable peer", func() {
-// 			It("should remain looking for a foreign peer to connect", func() {
-// 				successfulCallToSlaveNode := mockEtcdClient.EXPECT().GetLeader(gomock.Any()).SetArg("http://98.245.153.111:7001").Return("http://98.245.153.111:7001", nil)
-// 				mockEtcdClient.EXPECT().GetLeader(gomock.Any()).After(successfulCallToSlaveNode)
-// 			})
-// 		})
-// 	})
-// 	It("should send request to connect", func() {
-// 		mockEtcdClient.EXPECT().Set().Times(1)
-
-// 		go func() {
-// 			etcdSupervisor.Run()
-// 		}()
-// 	})
-// 	Context("when request to connect is accepted", func() {
-// 		It("should try to connect", func() {
-
-// 		})
-// 		Context("when connect to peer", func() {
-// 			It("should change its state to slave", func() {
-
-// 			})
-// 		})
-// 		Context("when connect to peer is impossible", func() {
-// 			It("should remain as master prima", func() {
-
-// 			})
-// 		})
-// 	})
-// 	Context("when request to connect is not accepted", func() {
-// 		It("should try to connect", func() {
-
-// 		})
-// 	})
-// })
-// 	})
-// })
